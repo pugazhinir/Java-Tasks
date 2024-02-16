68c9c738ce21d8b532f49d3703dc7f2bf771d2fa
pugazhinir <118433179+pugazhinir@users.noreply.github.com>
2024-02-16 19:44:15 +0530

    commit task file

---
+++ Variables.java
@@ -0,0 +1,22 @@
+class Variables{
+public static void main(String[]args){
+char c='$';
+boolean bool=false;
+byte b= -128;
+short s= 1;
+long l= 1L;
+float f= 10.5f;
+double d= 10.5d;
+int i =1;
+String Str= "welcome to Java world";
+System.out.println(c);
+System.out.println(bool);
+System.out.println(b);
+System.out.println(s);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
+System.out.println(i);
+System.out.println(Str);
+}
+}
