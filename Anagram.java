b0be670814617865890013ed23b74fa76cdd078c
pugazhinir <118433179+pugazhinir@users.noreply.github.com>
2024-03-17 21:16:28 +0530

    add revise task

---
+++ Anagram.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove spaces and convert both strings to lowercase
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // Check if the lengths of the strings are equal
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert both strings to character arrays
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        // Sort the character arrays
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        // Check if the sorted arrays are equal
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
---
+++ PerfectNumber.java
@@ -0,0 +1,24 @@
+public class PerfectNumber {
+
+    public static void main(String[] args) {
+        int number = 28; // Change this number to test different inputs
+
+        if (isPerfectNumber(number)) {
+            System.out.println(number + " is a perfect number.");
+        } else {
+            System.out.println(number + " is not a perfect number.");
+        }
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        int sum = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
+    }
+}
